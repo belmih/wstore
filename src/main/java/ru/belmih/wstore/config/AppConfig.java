280fb5b250cdd942df6a6371d2176dbb8369abca
mih <belmd@yandex.ru>
2017-09-23 20:37:35 +0300

    get username+roles from db with encrypt

--- src/main/java/ru/belmih/wstore/config/AppConfig.java
+++ src/main/java/ru/belmih/wstore/config/AppConfig.java
@@ -1,10 +1,18 @@
 package ru.belmih.wstore.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
@@ -12,7 +20,14 @@ import org.springframework.web.servlet.view.JstlView;
 @Configuration
 @ComponentScan({ "ru.belmih.wstore.*" })
 @Import({ SecurityConfig.class })
-public class AppConfig {
+public class AppConfig extends WebMvcConfigurerAdapter {
+
+	private static final Logger logger = LoggerFactory.getLogger(AppConfig.class);
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
 
 	@Bean
 	public InternalResourceViewResolver viewResolver() {
@@ -22,5 +37,24 @@ public class AppConfig {
 		viewResolver.setSuffix(".jsp");
 		return viewResolver;
 	}
+
+	@Bean(name = "dataSource")
+	public DriverManagerDataSource dataSource() {
+		DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+		driverManagerDataSource.setDriverClassName("org.h2.Driver");
+		driverManagerDataSource.setUrl("jdbc:h2:tcp://localhost/~/test");
+		driverManagerDataSource.setUsername("sa");
+		driverManagerDataSource.setPassword("");
+		return driverManagerDataSource;
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder(){
+		PasswordEncoder encoder = new BCryptPasswordEncoder();
+		logger.info(encoder.toString());
+		return encoder;
+	}
+
+
 	
 }
--- src/main/java/ru/belmih/wstore/config/SecurityConfig.java
+++ src/main/java/ru/belmih/wstore/config/SecurityConfig.java
@@ -2,18 +2,33 @@ package ru.belmih.wstore.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("user").password("123").roles("USER");
+		auth.jdbcAuthentication().dataSource(dataSource)
+				.passwordEncoder(passwordEncoder)
+				.usersByUsernameQuery("select username, password, enabled from users where username = ?")
+				.authoritiesByUsernameQuery("select username, role from user_roles where username = ?");
 	}
 
 	@Override
